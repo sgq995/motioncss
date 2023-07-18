bcc67a306df9735d11d8dd58e0815678112892ed
Sebastian G. Q <sgq995@gmail.com>
2023-07-18 09:50:50 -0500

    chore: domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  site: 'https://motioncss.com',
+  integrations: [tailwind(), solidJs()],
 });
