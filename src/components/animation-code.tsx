16b85c3ee78ba2fb77cd6713c4a6f0f7db728cf1
Sebastian G. Q <sgq995@gmail.com>
2023-07-18 14:11:44 -0500

    chore: styling

--- src/components/animation-code.tsx
+++ src/components/animation-code.tsx
@@ -3,7 +3,7 @@ import { code } from '../state/code';
 
 export const AnimationCode: Component = () => {
   return (
-    <pre class="overflow-x-auto rounded bg-gray-300 p-2">
+    <pre class="max-h-full overflow-auto rounded bg-gray-300 p-2">
       <code>{code.get()}</code>
     </pre>
   );
