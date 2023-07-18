0fabe2a99fd3d8af7d5e06d7f7239c76293daeef
Sebastian G. Q <sgq995@gmail.com>
2023-07-18 10:37:45 -0500

    chore: analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import solidJs from '@astrojs/solid-js';
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://motioncss.com',
-  integrations: [tailwind(), solidJs()],
+  integrations: [
+    tailwind(),
+    solidJs(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
